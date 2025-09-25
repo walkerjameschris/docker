97fcd40addf7539c222001b967eeff1e5745d4b9
Chris <127646133+walkerjameschris@users.noreply.github.com>
2025-09-24 20:28:46 -0400

    Update app.py

--- monitor/app.py
+++ monitor/app.py
@@ -1,19 +1,7 @@
 from shiny import App, ui, reactive, render
 
-def get_cpu_temp():
-    return "1"  # Replace with actual logic if needed
-
 app_ui = ui.page_fluid(
-    ui.card(
-        ui.output_text("temp"),
-        style="width: 200px; margin-top: 100px"
-    )
+    ui.card(1)
 )
 
-def server(input, output, session):
-
-    def temp():
-        return f"CPU Temp: {get_cpu_temp()}°C"
-
-app = App(app_ui, server)
-
+app = App(app_ui)
