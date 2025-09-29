2a9bb3acf1efb3892719fea2d7227910ba5d301f
Chris <127646133+walkerjameschris@users.noreply.github.com>
2025-09-29 19:17:02 -0400

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -1,17 +1,26 @@
 from chatlas import ChatOllama
 from shiny.express import ui
 
-# Might instead be ChatAnthropic, ChatOpenAI, or some other provider
 chat_client = ChatOllama(
     model="llama3",
     base_url="http://ollama:11434"
 )
 
-chat = ui.Chat(id="my_chat")
+ui.page_opts(
+    title="treehouse",
+    fillable=True,
+    fillable_mobile=True,
+)
+
+chat = ui.Chat(
+    id="chat",
+    messages=["Hello! How can I help you today?"],
+)
+
 chat.ui()
+
 
 @chat.on_user_submit
 async def handle_user_input(user_input: str):
     response = await chat_client.stream_async(user_input)
     await chat.append_message_stream(response)
-
