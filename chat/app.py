3203bf61d70e2b29fe2462581569dea541260b8d
Chris <127646133+walkerjameschris@users.noreply.github.com>
2025-09-29 19:50:06 -0400

    Update app.py

--- chat/app.py
+++ chat/app.py
@@ -1,5 +1,17 @@
 from chatlas import ChatOllama
 from shiny.express import ui
+from subprocess import run
+
+messages = [
+    {
+        "content": "You are a helpful general knowledge assistant running on a home server.",
+        "role": "system"
+    },
+    {
+        "content": "My name is Poplar, the `treehouse` assistant. Type \status to get server status",
+        "role": "assistant"
+    }
+]
 
 chat_client = ChatOllama(
     model="llama3",
@@ -7,20 +19,18 @@ chat_client = ChatOllama(
 )
 
 ui.page_opts(
-    title="treehouse",
     fillable=True,
-    fillable_mobile=True,
+    fillable_mobile=True
 )
 
-chat = ui.Chat(
-    id="chat",
-    messages=["Hello! How can I help you today?"],
-)
-
-chat.ui()
+chat = ui.Chat("poplar")
+chat.ui(messages=messages)
 
 
 @chat.on_user_submit
 async def handle_user_input(user_input: str):
+    if user_input == "\status":
+        gpu = run("nvidia-smi", capture_output=True)
+        user_input = f"Tell me about my GPU utilization in a nice list: {gpu}"
     response = await chat_client.stream_async(user_input)
     await chat.append_message_stream(response)
